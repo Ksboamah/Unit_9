6b79c856146899fa867cb9f9eb63a2e7cc4a1ce2
Kwame <bkwamesakyi@gmail.com>
2019-12-04 11:11:25 -0500

    I stared Unit_9. I started OOP daily exercises. I continued my daily excercises.

--- pack.py
+++ pack.py
@@ -1,12 +1,12 @@
-# Next create a new file in the same project called pack.py.
-# Create a Pack class.  The __init__ method should have one parameter, a Dog.
-# There should also be two properties:
-# a List called members to contain the different Dogs created and a leader_index
-# variable that is the index of the pack leader in the dog list.
-# The __init__ method should add the dog to the members list and set the leader_index to zero,
-# since that’s the index at which the leader will be placed.
-
-class Pack:
 import dog
 
+class Pack:
+
     def __init__(self, dog):
+        self.dog = dog
+        self.members = []
+        self.leader_index = 0
+        self.members.append(self.dog)
+
+    def get_leader_name(self):
+        return self.members[self.leader_index].get_name()
--- tested.py
+++ tested.py
@@ -1,5 +1,16 @@
 import dog
+import pack
 
 make_a_dog = dog.Dog("Randal")
 make_a_dog.print_name()
 make_a_dog.print_trick_list()
+
+dog_2 = dog.Dog("Junno")
+
+dog_3 = dog.Dog("Kinny")
+
+dog_4 = dog.Dog("Betty")
+
+
+make_a_pack_leader = pack.Pack(make_a_dog)
+print(make_a_pack_leader.get_leader_name())
